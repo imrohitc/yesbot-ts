6b69a80de8d957c9bacac3cb5897ede4c0cfea73
Hasan Latch <hassoune80@gmail.com>
2021-08-30 21:39:18 +0200

    split up functions

--- src/programs/index.ts
+++ src/programs/index.ts
@@ -3,7 +3,7 @@ import * as Game from "./game";
 import * as GroupManagerTools from "./group-manager";
 import GroupManager from "./group-manager";
 import * as MapTools from "./map";
-import * as VoiceOnDemandTools from "./voice-on-demand";
+import * as VoiceOnDemandTools from "./voice/voice-on-demand";
 import ExportManager from "./export-manager";
 import ReactRole from "./react-role";
 import Ticket from "./ticket";
