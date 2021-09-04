3714c0c1f79e8abb64ea0b1f4db90c80c9497294
Hasan Latch <hassoune80@gmail.com>
2021-09-04 15:56:15 +0200

    requested changes

--- src/programs/index.ts
+++ src/programs/index.ts
@@ -3,7 +3,6 @@ import * as Game from "./game";
 import * as GroupManagerTools from "./group-manager";
 import GroupManager from "./group-manager";
 import * as MapTools from "./map";
-import * as VoiceOnDemandTools from "./voice/voice-create";
 import ExportManager from "./export-manager";
 import ReactRole from "./react-role";
 import Ticket from "./ticket";
@@ -17,5 +16,4 @@ export {
   ReactRole,
   DailyChallenge,
   Ticket,
-  VoiceOnDemandTools,
 };
