c4b76b9754fe74be76f499ab3f897ed8d42c83ec
Hasan Latch <hassoune80@gmail.com>
2021-09-29 19:37:13 +0200

    WIP: refactor group.ts

--- src/programs/index.ts
+++ src/programs/index.ts
@@ -1,7 +1,7 @@
 import * as DailyChallenge from "./daily-challenge";
 import * as Game from "./game";
-import * as GroupManagerTools from "./group-manager";
-import GroupManager from "./group-manager";
+import * as GroupManagerTools from "./group-manager/group-manager";
+import GroupManager from "./group-manager/group-manager";
 import * as MapTools from "./map";
 import * as VoiceOnDemandTools from "./voice-on-demand";
 import VoiceOnDemand from "./voice-on-demand";
