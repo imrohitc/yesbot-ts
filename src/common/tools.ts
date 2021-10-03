14c30f531ed787ab745b4a738b8b1af1184a54ef
geisterfurz007 <michelvonv@me.com>
2021-10-03 17:27:44 +0200

    fix: axios typing

--- src/common/tools.ts
+++ src/common/tools.ts
@@ -130,7 +130,7 @@ class Tools {
     const apiKey = process.env.GOOGLE_API_KEY;
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/Sheet1?key=${apiKey}`;
 
-    const response = await axios.get(url);
+    const response = await axios.get<{ values: string[][] }>(url);
     const data = await response.data;
     const rows = data.values;
 
